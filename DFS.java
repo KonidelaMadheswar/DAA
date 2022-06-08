671484fe3bbfc6e4ca779e83b60886940f6d5f59
konidela madheswar <93138789+KonidelaMadheswar@users.noreply.github.com>
2022-06-08 18:00:51 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -49,3 +49,25 @@ class DFS {
         g.DFS(z);
     }
 }
+
+
+/*
+OUTPUT:
+-------
+Enter no. of edges : 12
+Enter edges :
+1 2
+2 1
+1 3
+3 1
+1 4
+4 1
+2 5
+5 2
+3 5
+5 3
+4 5
+5 4
+Enter Source Vertex : 1
+The DFS Traversal is 1 2 5 3 4
+*/
